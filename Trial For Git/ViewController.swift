9babe5dd3f77ade99e43fbd634b104065773a538
Jack Cookish <jcookish@umass.edu>
2021-09-14 13:04:03 -0400

    Initial Commit

---
+++ Trial For Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trial For Git
+//
+//  Created by Jack Cookish on 9/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
